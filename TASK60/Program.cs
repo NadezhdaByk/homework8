7e9ea48813bf88a04fb6165dab56275c65f341be
Nadezhda <nbykova79@gmail.com>
2022-07-22 18:21:03 +0300

    save task 60

--- TASK60/Program.cs
+++ TASK60/Program.cs
@@ -1,33 +1,12 @@
 ﻿// Формирует трёхмерный массив из неповторяющихся двузначных чисел. 
 // Построчно выводит массив, добавляя индексы каждого элемента.
-// Console.WriteLine("Введите кол-во строк");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите кол-во столбцов");
-// int n = Convert.ToInt32(Console.ReadLine());
 
-
-// void printArray(int[,,] inArray)
-// {
-//     for (int i = 0; i < inArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < inArray.GetLength(1); j++)
-//         {
-//             for (int z = 0; z < inArray.GetLength(2); z++)
-//             {
-//             Console.Write($"{inArray[i, j, z]} ({i},{j},{z}) ");
-//             }
-//         }
-//         Console.WriteLine();
-//     }
-// }
-//
 int [] SelectArray(int N)
 {
     int [] arr = new int [N];
     for (int i=1; i<N; i++)
     {
         arr[0]= new Random().Next(9,99);
-        //arr[1]=new Random().Next(9,99);
         for(int j=i-1; j>=0; j--)
         {
             if (arr[i]==arr[j]) 
@@ -57,14 +36,6 @@ int [,,] ResultArray(int[]inArray, int xlocal, int ylocal, int zlocal)
     }
     return array;
 }
-// void printArray1(int[] inArray)
-// {
-//     for (int i = 0; i < inArray.Length; i++)
-//     {
-//         Console.Write($"{inArray[i]}  ");
-//     }
-// }
-
 void printArray2(int[,,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
@@ -79,7 +50,6 @@ void printArray2(int[,,] inArray)
         Console.WriteLine();
     }
 }
-int [] array = SelectArray(8);
+int [] array = SelectArray(27);
 Console.WriteLine(string.Join(", ",array));
-//printArray1(array);
-printArray2(ResultArray(array,2,2,2));
+printArray2(ResultArray(array,3,3,3));
